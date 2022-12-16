fd5189f67d9a133314d81c10feb5bded1721352c
Yslan Ramos <yslanramos.r@gmail.com>
2022-12-16 19:52:32 -0300

    feat: setup eslint

--- sapphire-ts-fp/src/lib/env-parser.ts
+++ sapphire-ts-fp/src/lib/env-parser.ts
@@ -1,11 +1,14 @@
-import * as O from 'fp-ts/Option';
-import { pipe } from 'fp-ts/function';
+import * as O from "fp-ts/Option";
+import { pipe } from "fp-ts/function";
 
-export function envParseArray(key: 'OWNERS', defaultValue?: string[]): O.Option<string[]> {
-	return pipe(
-		process.env[key],
-		O.fromNullable,
-		O.map((str) => str.split(' ')),
-		O.alt(() => pipe(defaultValue, O.fromNullable))
-	);
+export function envParseArray(
+  key: "OWNERS",
+  defaultValue?: string[]
+): O.Option<string[]> {
+  return pipe(
+    process.env[key],
+    O.fromNullable,
+    O.map((str) => str.split(" ")),
+    O.alt(() => pipe(defaultValue, O.fromNullable))
+  );
 }
--- sapphire-ts-fp/src/ports/sapphire/listeners/mentionPrefixOnly.ts
+++ sapphire-ts-fp/src/ports/sapphire/listeners/mentionPrefixOnly.ts
@@ -1,9 +1,13 @@
-import { Listener } from '@sapphire/framework';
-import type { Message } from 'discord.js';
+import { Listener } from "@sapphire/framework";
+import type { Message } from "discord.js";
 
-export class UserEvent extends Listener<'mentionPrefixOnly'> {
-	public async run(message: Message) {
-		const prefix = this.container.client.options.defaultPrefix;
-		return message.channel.send(prefix ? `My prefix in this guild is: \`${prefix}\`` : 'Cannot find any prefix for Message Commands.');
-	}
+export class UserEvent extends Listener<"mentionPrefixOnly"> {
+  public async run(message: Message) {
+    const prefix = this.container.client.options.defaultPrefix;
+    return message.channel.send(
+      prefix
+        ? `My prefix in this guild is: \`${prefix}\``
+        : "Cannot find any prefix for Message Commands."
+    );
+  }
 }
